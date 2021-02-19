50770406f32c7a6f3fc3ba98e444dd6aac1d71b9
joesama <joharijumali@gmail.com>
2021-02-20 04:23:24 +0800

    Add requestCacheId trait

---
+++ src/Traits/RequestCacheId.php
@@ -0,0 +1,19 @@
+<?php
+namespace Lokalkoder\Support\Traits;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+trait RequestCacheId
+{
+    /**
+     * @param Request $request
+     * @return string
+     */
+    public function getCacheId(string $name, Request $request): string
+    {
+        $inputId = preg_replace('/[^A-Za-z0-9\-]/', '', json_encode(array_values($request->input())));
+
+        return $name . Auth::id() . $inputId;
+    }
+}
